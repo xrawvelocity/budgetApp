aadc3ded7108d37e1f193ae1bc529226ec8367a1
Victor Fernandez <vmfs99@gmail.com>
2020-07-17 19:53:46 -0400

    yes

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,12 +1,40 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export default class Login extends Component {
-    render() {
-        return (
-            <div className="login">
-                Login
-                
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="login">
+        <form className="form">
+          <h2 className="title">Login</h2>
+
+          <div className="group">
+            <input
+              type="email"
+              autocapitalize="off"
+              autocorrect="off"
+              name="MERGE0"
+              id="MERGE0"
+              size="25"
+              required
+            />
+            <span className="highlight"></span>
+            <span className="bar"></span>
+            <label>Email</label>
+          </div>
+
+          <div className="group">
+            <input type="password" name="MERGE1" id="MERGE1" size="25" required />
+            <span className="highlight"></span>
+            <span className="bar"></span>
+            <label>Password</label>
+          </div>
+
+          <div>
+            <input className="btn" type="submit" name="submit" value="Log In" />
+          </div>
+
+        </form>
+      </div>
+    );
+  }
 }
--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,11 +1,47 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export default class Signup extends Component {
-    render() {
-        return (
-            <div>
-                signup
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="login">
+        <form className="form">
+          <h2 className="title">Sign Up</h2>
+
+          <div className="group">
+            <input
+              type="email"
+              autocapitalize="off"
+              autocorrect="off"
+              name="MERGE0"
+              id="MERGE0"
+              size="25"
+              required
+            />
+            <span className="highlight"></span>
+            <span className="bar"></span>
+            <label>Email</label>
+          </div>
+
+          <div className="group">
+            <input type="password" name="MERGE1" id="MERGE1" size="25" required />
+            <span className="highlight"></span>
+            <span className="bar"></span>
+            <label>Password</label>
+          </div>
+
+          <div className="group">
+            <input type="password" name="MERGE1" id="MERGE1" size="25" required />
+            <span className="highlight"></span>
+            <span className="bar"></span>
+            <label>Confirm Password</label>
+          </div>
+
+          <div>
+            <input className="btn" type="submit" name="submit" value="Sign Up" />
+          </div>
+
+        </form>
+      </div>
+    );
+  }
 }
