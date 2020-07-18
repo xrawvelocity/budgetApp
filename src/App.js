65972aff0aa5fb4c644651afcbe697695c492444
Victor Fernandez <vmfs99@gmail.com>
2020-07-18 03:10:25 -0400

    git outta here

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { Route, Switch } from "react-router-dom";
-import Home from "./components/Home";
+// import Home from "./components/Home";
 import Profile from "./components/Profile";
 import Navigation from "./components/Navigation";
 import Signup from "./components/Signup";
